205a9721443611ac78135dbc5a78cd5172cac1c6
Oleksandr Sviatetskyi <alex.sviatetskyi@outlook.com>
2022-04-12 20:03:28 +0200

    Implement tests for Caesar's encoder

---
+++ CryptographicAlgorithms.Tests/CaesarEncoderTests.cs
@@ -0,0 +1,113 @@
+﻿using CryptographicAlgorithms.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CryptographicAlgorithms.Tests
+{
+    public class CaesarEncoderTests
+    {
+        #region Encode
+        [Theory]
+        [InlineData("AbC", 1, "BCD")]
+        [InlineData("AbC", -1, "ZAB")]
+        [InlineData("AbC", 0, "ABC")]
+        public void Encode_ShouldReturnEncodedMessage(string message, int shift, string expected)
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(shift);
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Encode_ShouldIgnoreNonLetters()
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(0);
+            string message = "A9B%34C";
+            string expected = "ABC";
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Encode_ShouldReturnEmptyStringForInvalidInput(string message)
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(0);
+            string expected = "";
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+     
+        #region Decode
+        [Theory]
+        [InlineData("BcD", 1, "ABC")]
+        [InlineData("zAb", -1, "ABC")]
+        [InlineData("AbC", 0, "ABC")]
+        public void Decode_ShouldReturnEncodedMessage(string message, int shift, string expected)
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(shift);
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Decode_ShouldIgnoreNonLetters()
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(0);
+            string message = "A9B%34C";
+            string expected = "ABC";
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Decode_ShouldReturnEmptyStringForInvalidInput(string message)
+        {
+            // Arrange
+            IEncoder encoder = new CaesarEncoder(0);
+            string expected = "";
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+    }
+}
---
+++ CryptographicAlgorithms/CaesarEncoder.cs
@@ -0,0 +1,28 @@
+﻿using CryptographicAlgorithms.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptographicAlgorithms
+{
+    public class CaesarEncoder : IEncoder
+    {
+        private readonly int _shift;
+
+        public CaesarEncoder(int shift)
+        {
+            _shift = shift;
+        }
+
+        public string Encode(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string encryptedMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
