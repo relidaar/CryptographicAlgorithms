1d38be9d010755de414fe1a04340544d5a8d753b
Oleksandr Sviatetskyi <alex.sviatetskyi@outlook.com>
2022-04-13 19:21:14 +0200

    Implement Polybius encoder

--- CryptographicAlgorithms/Helpers/EncoderHelper.cs
+++ CryptographicAlgorithms/Helpers/EncoderHelper.cs
@@ -25,6 +25,39 @@ namespace CryptographicAlgorithms.Helpers
 
         public static char[] GenerateAlphabet() => GetCharacters(65, 90);
 
+        public static char[,] GenerateMatrix(this char[] alphabet)
+        {
+            int matrixSize = (int)Math.Floor(Math.Sqrt(alphabet.Length));
+            var matrix = new char[matrixSize, matrixSize];
+            
+            for (uint i = 0, row = 0; row < matrixSize; row++)
+            {
+                for (var column = 0; column < matrixSize; column++)
+                {
+                    matrix[row, column] = alphabet[i];
+                    i++;
+                }
+            }
+
+            return matrix;
+        }
+
+        public static IDictionary<char, (uint row, uint column)> CreateIndicesCache(this char[,] alphabetMatrix)
+        {
+            var dict = new Dictionary<char, (uint, uint)>();
+            int size = alphabetMatrix.GetLength(0);
+            for (uint row = 0; row < size; row++)
+            {
+                for (uint column = 0; column < size; column++)
+                {
+                    char letter = alphabetMatrix[row, column];
+                    dict[letter] = (row, column);
+                }
+            }
+            return dict;
+        }
+
+
         public static string ReplaceAt(this string value, uint index, char newChar)
         {
             if (value == null || index >= value.Length) return value;
--- CryptographicAlgorithms/PolybiusEncoder.cs
+++ CryptographicAlgorithms/PolybiusEncoder.cs
@@ -10,18 +10,58 @@ namespace CryptographicAlgorithms
 {
     public class PolybiusEncoder : IEncoder
     {
+        private readonly char _letterToRemove = 'J';
+        private readonly char[] _alphabet;
+        private readonly char[,] _keyMatrix;
+        private readonly int _keyMatrixSize;
+        private readonly IDictionary<char, (uint row, uint column)> _indicesCache;
+
         public PolybiusEncoder(char letterToRemove = 'J')
         {
+            var alphabet = EncoderHelper.GenerateAlphabet();
+            letterToRemove = char.ToUpper(letterToRemove);
+
+            if (alphabet.Contains(letterToRemove) == false)
+                throw new ArgumentException("The chosen letter has to be from Latin alphabet.");
+
+            _letterToRemove = letterToRemove;
+            _alphabet = alphabet.Where(c => c != letterToRemove).ToArray();
+            _keyMatrix = _alphabet.GenerateMatrix();
+            _keyMatrixSize = _keyMatrix.GetLength(0);
+            _indicesCache = _keyMatrix.CreateIndicesCache();
         }
 
         public string Decode(string encryptedMessage)
         {
-            throw new NotImplementedException();
+            encryptedMessage = encryptedMessage ?? string.Empty;
+            var digits = encryptedMessage
+                .Where(c => char.IsDigit(c))
+                .Select(d => (d - '0') - 1)
+                .Where(d => d >= 0 && d < _keyMatrixSize)
+                .ToArray();
+
+            var indices = Enumerable
+                .Range(0, digits.Length / 2)
+                .Select(i => 
+                {
+                    var pair = digits.Skip(i * 2).Take(2);
+                    int row = pair.First();
+                    int column = pair.Last();
+                    return (row, column);
+                });
+
+            var output = indices.Select(i => _keyMatrix[i.row, i.column]);
+            return string.Concat(output);
         }
 
         public string Encode(string message)
         {
-            throw new NotImplementedException();
+            var output = message
+                .Filter(_alphabet)
+                .Where(c => c != _letterToRemove)
+                .Select(c => _indicesCache[c])
+                .Select(i => $"{i.row + 1}{i.column + 1}");
+            return string.Concat(output);
         }
     }
 }
