c65b57f03cd43913834c0778fd51aada1c3ef9fc
Oleksandr Sviatetskyi <alex.sviatetskyi@outlook.com>
2022-04-13 16:31:26 +0200

    Implement tests for Polybius encoder

---
+++ CryptographicAlgorithms.Tests/PolybiusEncoderTests.cs
@@ -0,0 +1,134 @@
+﻿using CryptographicAlgorithms.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CryptographicAlgorithms.Tests
+{
+    using Encoder = PolybiusEncoder;
+    public class PolybiusEncoderTests
+    {
+        #region Constructor
+        [Theory]
+        [InlineData('1')]
+        [InlineData('%')]
+        public void Constructor_ShouldRaiseExceptionForInvalidLetter(char letterToRemove)
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Throws<ArgumentException>(() => new Encoder(letterToRemove));
+        }
+        #endregion
+
+        #region Encode
+        [Theory]
+        [InlineData("AbC", "111213")]
+        [InlineData("EncryptedKeyword", "15331342543544151425155452344214")]
+        [InlineData("EncryptedKeyword", "153313425435441514155452344214", 'K')]
+        public void Encode_ShouldReturnEncodedMessage(
+            string message,
+            string expected,
+            char letterToRemove = 'J')
+        {
+            // Arrange
+            IEncoder encoder = new Encoder(letterToRemove);
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Encode_ShouldIgnoreNonLetters()
+        {
+            // Arrange
+            IEncoder encoder = new Encoder();
+            string message = "A9B%34C";
+            string expected = "111213";
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Encode_ShouldReturnEmptyStringForInvalidInput(string message)
+        {
+            // Arrange
+            IEncoder encoder = new Encoder();
+            string expected = "";
+
+            // Act
+            string actual = encoder.Encode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+        #region Decode
+        [Theory]
+        [InlineData("ABC", "111213")]
+        [InlineData("ENCRYPTEDKEYWORD", "15331342543544151425155452344214")]
+        [InlineData("ENCRYPTEDEYWORD", "153313425435441514155452344214", 'K')]
+        public void Decode_ShouldReturnEncodedMessage(
+            string expected,
+            string message,
+            char letterToRemove = 'J')
+        {
+            // Arrange
+            IEncoder encoder = new Encoder(letterToRemove);
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Decode_ShouldIgnoreNonLetters()
+        {
+            // Arrange
+            IEncoder encoder = new Encoder();
+            string message = "111%213C";
+            string expected = "ABC";
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Decode_ShouldReturnEmptyStringForInvalidInput(string message)
+        {
+            // Arrange
+            IEncoder encoder = new Encoder();
+            string expected = "";
+
+            // Act
+            string actual = encoder.Decode(message);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+    }
+}
---
+++ CryptographicAlgorithms/PolybiusEncoder.cs
@@ -0,0 +1,27 @@
+﻿using CryptographicAlgorithms.Helpers;
+using CryptographicAlgorithms.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryptographicAlgorithms
+{
+    public class PolybiusEncoder : IEncoder
+    {
+        public PolybiusEncoder(char letterToRemove = 'J')
+        {
+        }
+
+        public string Decode(string encryptedMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encode(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
